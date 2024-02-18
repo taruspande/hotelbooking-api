660c85ee812a6323ee6c7d0361a2e7de2ad095ed
shivNarang <23B1075@iitb.ac.in>
2024-02-18 11:42:20 +0530

    hotels added

---
+++ hotels/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+import uuid
+
+class Hotel(models.Model):
+    hotel_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    contact = models.CharField(max_length=20)
+    free_wifi = models.BooleanField(default=False)
+    pool = models.BooleanField(default=False)
+    spa = models.BooleanField(default=False)
+    restaurant = models.BooleanField(default=False)
+    laundry = models.BooleanField(default=False)
+    checkin_time = models.TimeField()
+    checkout_time = models.TimeField()
