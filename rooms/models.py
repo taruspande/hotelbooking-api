99c8b0cd08eccf3a6c523709949365bc6efe5bff
shivNarang <23B1075@iitb.ac.in>
2024-02-18 11:51:50 +0530

    HI

---
+++ rooms/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from hotels.models import Hotel
+import uuid
+
+class Room(models.Model):
+    room_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    room_type = models.CharField(max_length=100)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
