99642df1b08825b9c11bff0b3f740ba5285014ee
Arnav Bhate <bhatearnav@gmail.com>
2024-03-02 23:08:36 +0530

    Send errors in response whenever possible

--- rooms/views.py
+++ rooms/views.py
@@ -26,7 +26,7 @@ def CreateView(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -52,7 +52,7 @@ def UpdateView(request, room_id):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
--- user/views.py
+++ user/views.py
@@ -11,9 +11,10 @@ from rest_framework_simplejwt.tokens import RefreshToken
 def RegisterView(request):
     try:
         serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
