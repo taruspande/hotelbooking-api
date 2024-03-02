efda8e7990c32fb4bcf00dfa613bb5dbb8df29fc
Arnav Bhate <bhatearnav@gmail.com>
2024-03-02 21:46:09 +0530

    Added ListView

---
+++ rooms/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+
+"""
+API endpoints for rooms
+api/rooms/list/ - GET request
+    Optional fields: hotel_id
+    Returns: list of rooms
+"""
+urlpatterns = [
+    path("list/", ListView, name="list"),
+]
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 
 # Create your views here.
+@api_view(http_method_names=["GET"])
+def ListView(request):
+    try:
+        hotel_id = request.query_params.get("hotel_id")
+        if hotel_id:
+            rooms = Room.objects.filter(hotel_id=hotel_id)
+        else:
+            rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
