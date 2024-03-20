a8c4109f8b7f6bbd6e47c1eec062645ba78b1066
Arnav Bhate <bhatearnav@gmail.com>
2024-03-20 23:38:08 +0530

    Implemented filtering in hotel_list api

--- hotels/views.py
+++ hotels/views.py
@@ -5,11 +5,75 @@ from .serializers import *
 from rooms.models import Room
 
 
+def gen_keywords(name):
+    min_word_length = 3
+    avoid = [
+        "the",
+        "to",
+        "i",
+        "am",
+        "is",
+        "are",
+        "he",
+        "she",
+        "a",
+        "an",
+        "and",
+        "here",
+        "there",
+        "can",
+        "could",
+        "were",
+        "has",
+        "have",
+        "had",
+        "been",
+        "of",
+        "into",
+        "this",
+        "there",
+    ]
+    name = name.lower()
+    words = name.split()
+    keywords = []
+    for word in words:
+        if len(word) >= min_word_length and word not in avoid:
+            keywords.append(word)
+    return keywords
+
+
 @api_view(["GET"])
 def hotel_list(request):
     try:
-        hotels = Hotel.objects.all()
-        serializer = HotelSerializer(hotels, many=True)
+        filters = {}
+        if "city" in request.query_params:
+            filters["city"] = request.query_params["city"]
+        if "stars" in request.query_params:
+            stars = eval(request.query_params["stars"])
+        else:
+            stars = [1, 2, 3, 4, 5]
+        if "free_wifi" in request.query_params:
+            filters["free_wifi"] = eval(request.query_params["free_wifi"])
+        if "pool" in request.query_params:
+            filters["pool"] = eval(request.query_params["pool"])
+        if "spa" in request.query_params:
+            filters["spa"] = eval(request.query_params["spa"])
+        if "gym" in request.query_params:
+            filters["gym"] = eval(request.query_params["gym"])
+        if "restaurant" in request.query_params:
+            filters["restaurant"] = eval(request.query_params["restaurant"])
+        if "laundry" in request.query_params:
+            filters["laundry"] = eval(request.query_params["laundry"])
+        hotels = Hotel.objects.filter(**filters, stars__in=stars)
+        if "name" in request.query_params:
+            name = request.query_params["name"]
+            keywords = gen_keywords(name)
+            hotel_list = []
+            for hotel in hotels:
+                hotel_keywords = gen_keywords(hotel.name)
+                if all(keyword in hotel_keywords for keyword in keywords):
+                    hotel_list.append(hotel)
+        serializer = HotelSerializer(hotel_list, many=True)
         for hotel in serializer.data:
             id = hotel["hotel_id"]
             rooms = Room.objects.filter(hotel_id=id)
